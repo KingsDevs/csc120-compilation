8a9bd26afae7955fc83be718252e779099ebd1d9
KingsDevs <sunnyckair@gmail.com>
2023-06-17 11:15:51 +0800

    first

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/rl')
+def serve_webgl():
+    return render_template("rl-game.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
