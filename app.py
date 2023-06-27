bdad51ddfd6d95b8a4ab8beacbb329c6994ae49a
juvimieretiza <retizajuvimie@gmail.com>
2023-06-27 15:16:20 +0800

    put pages

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', title="CSC 120 Compilation")
+    return render_template('index.html', title="CSC 120 COMPILATION")
 
 @app.route('/linear-regression')
 def lr():
